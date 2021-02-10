1854be50d84e4fb6814bbe596ed25229d3108adb
Berkay Gürbüz <berkay-gurbuz@hotmail.com>
2021-02-10 19:14:32 +0300

    add axios package and update showevent.vue

--- #Route Example/src/views/EventList.vue
+++ #Route Example/src/views/EventList.vue
@@ -2,17 +2,34 @@
     <div>
         <h1>Event Listing</h1>
 
-        <EventCard/>
+        <EventCard v-for="event in events" :key="event.id" :event="event" />
         <BaseIcon/>
     </div>
 </template>
 
 <script>
 import EventCard from '../components/EventCard.vue';
+import EventService from '../services/EventServices.js';
 
 export default {
+
     components:{
         EventCard
+    },    
+    data(){
+        return{
+            events:[],
+        }
+    },
+    created(){
+        EventService.getEvents()
+        .then(response=>{
+            console.log(response.data);
+            this.events=response.data
+        })
+        .catch(error=>{
+            console.log("There was an errors"+error.response);
+        })
     }
 }
 </script>
