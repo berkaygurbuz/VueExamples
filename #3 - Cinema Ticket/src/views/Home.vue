5d5cab9ea27478f52ec83e4cd982d6979fa60570
Berkay Gürbüz <berkay-gurbuz@hotmail.com>
2021-02-13 18:32:38 +0300

    init

---
+++ #3 - Cinema Ticket/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+   <h3>This is a home page</h3>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
