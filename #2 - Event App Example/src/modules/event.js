cd65d9c06e9ecddbd7c4afff267dc503ad3996e7
Berkay Gürbüz <berkay-gurbuz@hotmail.com>
2021-02-16 19:18:12 +0300

    add base components for createEvent view

--- #2 - Event App Example/src/modules/event.js
+++ #2 - Event App Example/src/modules/event.js
@@ -1,12 +1,12 @@
 import EventService from "../services/EventServices.js"
 
-export const namespaced = true;
+export const namespaced = true
 
 export const state = {
   events: [],
   eventsTotal: 0,
   event: {},
-  perPage:3,
+  perPage: 3
 }
 
 export const mutations = {
@@ -29,6 +29,7 @@ export const actions = {
     return EventService.postEvent(event)
       .then(() => {
         commit('ADD_EVENT', event)
+        commit('SET_EVENT', event)
         const notification = {
           type: 'success',
           message: 'Your event has been created!'
@@ -44,7 +45,7 @@ export const actions = {
         throw error
       })
   },
-  fetchEvents({ commit, dispatch }, { page }) {
+  fetchEvents({ commit, dispatch, state }, { page }) {
     return EventService.getEvents(state.perPage, page)
       .then(response => {
         commit('SET_EVENTS_TOTAL', parseInt(response.headers['x-total-count']))
@@ -58,25 +59,21 @@ export const actions = {
         dispatch('notification/add', notification, { root: true })
       })
   },
-  fetchEvent({ commit, getters, dispatch }, id) {
+  fetchEvent({ commit, getters, state }, id) {
+    if (id == state.event.id) {
+      return state.event
+    }
+
     var event = getters.getEventById(id)
 
     if (event) {
       commit('SET_EVENT', event)
       return event
     } else {
-      EventService.getEvent(id)
-        .then(response => {
-          commit('SET_EVENT', response.data)
-          return response.data;
-        })
-        .catch(error => {
-          const notification = {
-            type: 'error',
-            message: 'There was a problem fetching event: ' + error.message
-          }
-          dispatch('notification/add', notification, { root: true })
-        })
+      return EventService.getEvent(id).then(response => {
+        commit('SET_EVENT', response.data)
+        return response.data
+      })
     }
   }
 }
--- #2 - Event App Example/src/views/EventCreate.vue
+++ #2 - Event App Example/src/views/EventCreate.vue
@@ -2,27 +2,27 @@
   <div>
     <h1>Create an Event</h1>
     <form @submit.prevent="createEvent">
-      <label>Select a category</label>
-      <select v-model="event.category">
-        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
-      </select>
+      <BaseSelect label="Select a category" :options="categories" v-model="event.category"/>
 
       <h3>Name & describe your event</h3>
-      <div class="field">
-        <label>Title</label>
-        <input v-model="event.title" type="text" placeholder="Add an event title"/>
-      </div>
+      <BaseInput label="Title" v-model="event.title" type="text" placeholder="Title" class="field"/>
 
-      <div class="field">
-        <label>Description</label>
-        <input v-model="event.description" type="text" placeholder="Add a description"/>
-      </div>
+      <BaseInput
+        label="Description"
+        v-model="event.description"
+        type="text"
+        placeholder="Description"
+        class="field"
+      />
 
       <h3>Where is your event?</h3>
-      <div class="field">
-        <label>Location</label>
-        <input v-model="event.location" type="text" placeholder="Add a location"/>
-      </div>
+      <BaseInput
+        label="Location"
+        v-model="event.location"
+        type="text"
+        placeholder="Location"
+        class="field"
+      />
 
       <h3>When is your event?</h3>
 
@@ -31,14 +31,9 @@
         <datepicker v-model="event.date" placeholder="Select a date"/>
       </div>
 
-      <div class="field">
-        <label>Select a time</label>
-        <select v-model="event.time">
-          <option v-for="time in times" :key="time">{{ time }}</option>
-        </select>
-      </div>
+      <BaseSelect label="Select a time" :options="times" v-model="event.time" class="field"/>
 
-      <input type="submit" class="button -fill-gradient" value="Submit"/>
+       <BaseButton type="submit" buttonClass="-fill-gradient">Submit</BaseButton>
     </form>
   </div>
 </template>
@@ -46,11 +41,16 @@
 
 <script>
 import Datepicker from 'vuejs-datepicker'
-import NProgress from 'nprogress';
-
+import NProgress from 'nprogress'
+import BaseInput from '../components/BaseInput.vue'
+import BaseSelect from '../components/BaseSelect.vue'
+import BaseButton from '../components/BaseButton.vue'
 export default {
   components: {
-    Datepicker
+    Datepicker,
+    BaseInput,
+    BaseSelect,
+    BaseButton
   },
   data() {
     const times = []
@@ -65,7 +65,7 @@ export default {
   },
   methods: {
     createEvent() {
-      NProgress.start();
+      NProgress.start()
       this.$store
         .dispatch('event/createEvent', this.event)
         .then(() => {
@@ -76,7 +76,7 @@ export default {
           this.event = this.createFreshEventObject()
         })
         .catch(() => {
-          NProgress.done();
+          NProgress.done()
         })
     },
     createFreshEventObject() {
@@ -98,9 +98,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.field {
-  margin-bottom: 24px;
-}
-</style>
