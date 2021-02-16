7f737a95d35be4e4099c78778269f3120e9cafff
Berkay Gürbüz <berkay-gurbuz@hotmail.com>
2021-02-16 16:54:06 +0300

    add route-guards

--- #2 - Event App Example/src/router/index.js
+++ #2 - Event App Example/src/router/index.js
@@ -3,34 +3,48 @@ import VueRouter from "vue-router";
 import EventCreate from "../views/EventCreate.vue";
 import EventList from "../views/EventList.vue";
 import EventShow from "../views/EventShow.vue";
+import NProgress from 'nprogress';
+import store from '../store/index.js';
 
 
 Vue.use(VueRouter);
 
-const routes = [
-  
-  {
-    path: "/",
-    name: "event-list",
-    component: EventList
-  },
-  {
-    path: "/event/:id",
-    name: "event-show",
-    component:EventShow,
-    props:true
-  },
-  {
-    path:"event/create",
-    name:"event-create",
-    component:EventCreate
-  }
-
-];
-
 const router = new VueRouter({
-  mode:'history',
-  routes
-});
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'event-list',
+      component: EventList,
+      props:true,
+    },
+    {
+      path: '/event/create',
+      name: 'event-create',
+      component: EventCreate
+    },
+    {
+      path: '/event/:id',
+      name: 'event-show',
+      component: EventShow,
+      props: true,
+      beforeEnter(routeTo, routeFrom, next) {
+        store.dispatch('event/fetchEvent', routeTo.params.id).then(event => {
+          routeTo.params.event = event
+          next()
+        })
+      }
+    }
+  ]
+})
 
-export default router;
+router.beforeEach((routeTo, routeFrom, next) => {
+  NProgress.start()
+  next()
+})
+
+router.afterEach(() => {
+  NProgress.done()
+})
+
+export default router
