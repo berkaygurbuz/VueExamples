6a7efc4fc804b7277e57d6cdf121eb35fbc2c356
Berkay Gürbüz <berkay-gurbuz@hotmail.com>
2021-02-16 17:26:55 +0300

    add error handling

--- #2 - Event App Example/src/router/index.js
+++ #2 - Event App Example/src/router/index.js
@@ -5,6 +5,9 @@ import EventList from "../views/EventList.vue";
 import EventShow from "../views/EventShow.vue";
 import NProgress from 'nprogress';
 import store from '../store/index.js';
+import NotFound from '../views/NotFound.vue';
+import NetworkIssue from '../views/NetworkIssue.vue';
+
 
 
 Vue.use(VueRouter);
@@ -29,11 +32,33 @@ const router = new VueRouter({
       component: EventShow,
       props: true,
       beforeEnter(routeTo, routeFrom, next) {
-        store.dispatch('event/fetchEvent', routeTo.params.id).then(event => {
+        store.dispatch('event/fetchEvent', routeTo.params.id)
+        .then(event => {
           routeTo.params.event = event
           next()
+        }).catch(error => {
+          if (error.response && error.response.status == 404) {
+            next({ name: '404', params: { resource: 'event' } })
+          } else {
+            next({ name: 'network-issue' })
+          }
         })
       }
+    },
+    {
+      path:'/404',
+      name:'404',
+      component:NotFound,
+      props:true,
+    },
+    {
+      path:'*', //catch all not-found route
+      redirect:{name:'404',params:{resource:'page'}},
+    },
+    {
+      path: '/network-issue',
+      name: 'network-issue',
+      component: NetworkIssue,
     }
   ]
 })
