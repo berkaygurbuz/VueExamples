ffb496200419d8a11728d7feb51d7f45648ec8a6
Berkay Gürbüz <berkay-gurbuz@hotmail.com>
2021-02-18 16:46:59 +0300

    use vuex

--- 010 - Vue with Nuxt.js/nuxt.config.js
+++ 010 - Vue with Nuxt.js/nuxt.config.js
@@ -16,6 +16,10 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
+  loading:{
+    color:'#39b982'
+  },
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
--- 010 - Vue with Nuxt.js/pages/index.vue
+++ 010 - Vue with Nuxt.js/pages/index.vue
@@ -7,23 +7,29 @@
 
 <script>
 import EventCard from '../components/EventCard.vue';
+import {mapState} from 'vuex';
+
 export default {
     head() {
       return{
         title:'Event listing'
       }
     },
-    asyncData({$axios,error}){
-      return $axios.get('http://localhost:3000/events').then(response=>{
-        return{
-          events:response.data,
-        }
-      }).catch(e=>{
+    async fetch({store,error}){
+      try{
+       await store.dispatch('events/fetchEvents');
+      }
+      catch(e){
         error({statusCode:503,message:'Unable to fetch events at this time, please try again.'});
-      })
-    },
+      }
+      },
     components:{
       EventCard,
+    },
+    computed:{
+      ...mapState({
+        events:state=> state.events.events
+      })
     }
 }
 </script>
